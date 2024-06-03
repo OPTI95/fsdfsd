cb0e6f5b08f29a423713494b378887b51fbc9e16
Apti Amerkhanov <95killboy95@gmail.com>
2024-06-03 14:24:56 +0300

    first commit

---
+++ demo/demo/AddOrder.xaml.cs
@@ -0,0 +1,71 @@
+﻿using demo.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrder.xaml
+    /// </summary>
+    public partial class AddOrder : Window
+    {
+        public AddOrder()
+        {
+            InitializeComponent();
+            demoEntities demoEntities = new demoEntities();
+            List<string> types = new List<string> {"Сильное", "Среднее", "Слабое"};
+            typeBox.ItemsSource = types;
+            var objects = demoEntities.Object.ToList();
+            List<String> nameObject = new List<String>();
+            foreach (var item in objects)
+            {
+                nameObject.Add(item.name_object);  
+            }
+            objectBox.ItemsSource = nameObject;
+            var users = demoEntities.Users.ToList().Where(u => u.role_id == 2).ToList();
+            List<int> ints = new List<int>();
+            users.ForEach(u => ints.Add(u.id_user));
+            ispBox.ItemsSource = ints;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            demoEntities demoEntities = new demoEntities();
+            int idObject = demoEntities.Object.FirstOrDefault(u=> u.name_object == objectBox.SelectedValue.ToString()).id_object;
+            Random rand = new Random();
+            int number = rand.Next(0, 1000);
+            Order order = new Order(0,number , DateTime.Now, "Начал", LoginWindow.userID, null, problemBox.Text, typeBox.SelectedValue.ToString(), idObject, null);
+            demoEntities.Order.Add(order);
+            demoEntities.SaveChanges();
+            int orderID = demoEntities.Order.FirstOrDefault(u => u.number == number).id_order;
+            demoEntities.order_ispolnitel.Add(new order_ispolnitel(0, int.Parse(ispBox.SelectedValue.ToString()), orderID));
+            demoEntities.SaveChanges();
+            MessageBox.Show("Создан заказ", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                image.Source = new BitmapImage(fileUri);
+
+            }
+        }
+    }
+}
---
+++ demo/demo/LoginWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using demo.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public static int userID = 0;
+        demoEntities demoEntities;
+        public LoginWindow()
+        {
+            InitializeComponent();
+            demoEntities = new demoEntities();
+            AddOrder addOrder = new AddOrder();
+            addOrder.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Users user = demoEntities.Users.FirstOrDefault(u => u.login == loginBox.Text);
+            if (user == null)
+            {
+                MessageBox.Show("Пользователя нет в базе!", "Ошибка при авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (passwordBox.Password != user.password)
+                {
+                    MessageBox.Show("Пароль неверный!", "Ошибка при авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+                else
+                {
+                    userID = user.id_user;
+                    if (user.role_id == 1)
+                    {
+                        MessageBox.Show("Клиент!", "Ошибка при авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                        AddOrder addOrder = new AddOrder();
+                        addOrder.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Исполнитель!", "Ошибка при авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    }
+                }
+            }
+        }
+    }
+}
